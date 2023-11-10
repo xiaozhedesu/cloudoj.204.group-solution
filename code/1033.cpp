8a16d37b88363f07921f4e4645717c629bbb608b
xiaozhenano <xiaozhedesu0219@outlook.com>
2023-11-10 23:57:27 +0800

    P1033 倒转字符串 终于是把这题做完了是吧） 肯想总归是有做法的 （最优路径除外（）

--- code/1033.cpp
+++ code/1033.cpp
@@ -1,11 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 
+void printWord(char string[],int start,int end) {
+	for(int i = start; i <= end; i++) {
+		putchar(string[i]);
+	}
+}
+
 int main() {
 	char string[100];
-	do {
-		scanf("%s\n", &string);
-	} while (getchar() == ' ');
+	gets(string);
+
+	int length = strlen(string);
+	int stringEnd = length - 1;
+	//记录结尾字符，每扫到空格就打印这个单词，然后改变结尾位置继续扫
+	for(int num = length - 1; num > 0; num--) {
+		if(*(string + num) == ' ') {
+			printWord(string, num + 1, stringEnd);
+			stringEnd = num - 1;
+			putchar(' ');
+		}
+	}
+	//打印最后一个（第一个）字符
+	printWord(string,0,stringEnd);
 
 	return 0;
 }
